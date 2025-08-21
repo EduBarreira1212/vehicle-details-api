ce7c403c940e371d62de24b9a940770b83e0ebb2
Eduardo Barreira <edubarreira1212@gmail.com>
2025-08-20 22:43:54 -0300

    feat: create a route for test gin setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("App entry point")
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, Gin!",
+		})
+	})
+
+	router.Run(":8080")
 }
