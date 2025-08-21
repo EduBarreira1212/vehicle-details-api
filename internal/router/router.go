84330f91dba99049d623ed9d2425e747e985d035
Eduardo Barreira <edubarreira1212@gmail.com>
2025-08-21 17:25:38 -0300

    feat: implement router with API endpoints for cars

---
+++ internal/router/router.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func BuildRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) { c.String(200, "Ok!") })
+
+	api := r.Group("/api")
+
+	api.GET("/cars", func(c *gin.Context) { c.JSON(200, gin.H{"cars": []string{"car1", "car2", "car3"}}) })
+	api.GET("/cars/:id", func(c *gin.Context) { c.JSON(200, gin.H{"car": 123}) })
+
+	return r
+}
