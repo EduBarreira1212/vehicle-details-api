51c67baaa667859bfb42570acb8edaa1ee755abd
Eduardo Barreira <edubarreira1212@gmail.com>
2025-08-21 17:38:16 -0300

    fix: update import to use http package for router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"log"
 
-	"github.com/EduBarreira1212/vehicle-details-api/internal/router"
+	"github.com/EduBarreira1212/vehicle-details-api/internal/http"
 )
 
 func main() {
-	router := router.BuildRouter()
+	router := http.BuildRouter()
 
 	log.Println("listening on port 8080")
 	if err := router.Run(":8080"); err != nil {
