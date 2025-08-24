0b54b313238baafd180c75bc403e5cfe0e8aff9e
Eduardo Barreira <edubarreira1212@gmail.com>
2025-08-24 20:29:24 -0300

    fix: update User struct to include timestamps and change History type to datatypes.JSON

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,8 +1,19 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/datatypes"
+	"gorm.io/gorm"
+)
+
 type User struct {
-	Name     string   `json:"name"`
-	Email    string   `json:"email"`
-	Password string   `json:"password"`
-	History  []string `json:"history"`
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	Name      string         `json:"name"`
+	Email     string         `gorm:"uniqueIndex;size:180;not null" json:"email"`
+	Password  string         `json:"password"`
+	History   datatypes.JSON `json:"history"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
